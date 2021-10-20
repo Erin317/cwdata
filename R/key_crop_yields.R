e870e8feebfbd235a7697485389e71d3795c7e5c
Xinying Xu <xxuu0075@student.monash.edu>
2021-10-20 21:39:01 +0800

    add READ.Rmd and documentation

---
+++ R/key_crop_yields.R
@@ -0,0 +1,17 @@
+
+
+#' Title
+#'@section Background:
+#'The data was collected from ...over course
+#'
+#'
+#'@format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#'
+#' @examples
+#' print(key_crop_yields)
+"key_crop_yields"
